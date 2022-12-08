7ea4e4831163d1d9b591e5dbf79058406f1d13bc
Nan <nan_guo@hotmail.com>
2022-12-08 14:59:06 -0800

    pokemon get routes

--- flask-backend/app/routes/pokemon.py
+++ flask-backend/app/routes/pokemon.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, render_template, redirect
+# from flask import Blueprint, render_template, redirect
 
 
-from ..models import db,Pokemon
+# from ..models import db,Pokemon
 
 
-bp = Blueprint('pokemon', __name__, url_prefix='/api/pokemon' )
+# bp = Blueprint('pokemon', __name__, url_prefix='/api/pokemon' )
 
-@bp.route('/types')
-def get_types():
-    pokemon = Pokemon.query.all()
-    return pokemon.to_dict()
+# @bp.route('/types')
+# def get_types():
+#     pokemon = Pokemon.query.all()
+#     return pokemon.to_dict()
